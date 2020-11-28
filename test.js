dd5ad2b507422e94bff00f4e5353c4ae6fb247d7
lovelikeyou870405 <lovelikeyou870405@users.noreply.github.com>
2020-11-28 10:36:22 +0800

    2020/11/28

---
+++ test.js
@@ -0,0 +1,88 @@
+var btn = document.getElementsByClassName("product-variation");
+var btn2 = document.getElementsByClassName("btn-solid-primary");
+btn[3].click();
+btn2[1].click();
+if(btn[8].className=="product-variation product-variation--disabled")
+{
+    //不能按:
+    console.log('true');
+}
+else
+{
+    //能按:
+    console.log('false');
+}
+
+
+
+
+
+
+
+
+var timeout = 2;
+current = location.href;
+if(timeout > 0)
+{
+　　setTimeout('reload()', 1000 * timeout);
+}
+else
+{
+　　location.replace(current);
+}
+
+function reload()
+{
+　　setTimeout('reload()', 1000 * timeout);
+　　var frame = '<frameset cols=\'*\'>\n<frame src=\'' + current + '\' /></frameset>';
+    var btn = document.getElementsByClassName("product-variation");
+　　with(document){
+        write(frame);
+        console.log(btn);
+        void(close());
+        var btn3 = window.document.getElementsByClassName("product-variation");
+        console.log(btn3);
+　　};
+}
+
+
+// #1:
+var btn = document.getElementsByClassName("product-variation");
+var btn2 = document.getElementsByClassName("btn-solid-primary");
+btn[8].click();
+btn2[1].click();
+// #1.測試:
+var btn = document.getElementsByClassName("product-variation");
+var btn2 = document.getElementsByClassName("btn-solid-primary");
+btn[3].click();
+btn2[1].click();
+
+// #2:
+var btn3 = document.getElementsByClassName("cart-page-footer__checkout-text");
+btn3[0].click();
+
+// #3:
+var btn4 = document.getElementsByClassName("product-variation");
+btn4[2].click();
+var btn5 = document.getElementsByClassName("stardust-radio-button__inner-circle");
+btn5[3].click();
+var btn6 = document.getElementsByClassName("stardust-button");
+btn6[1].click();
+
+// #3.測試:
+var btn4 = document.getElementsByClassName("product-variation");
+btn4[2].click();
+var btn5 = document.getElementsByClassName("stardust-radio-button__inner-circle");
+btn5[3].click();
+var btn6 = document.getElementsByClassName("stardust-button");
+console.log(btn6[1]);
+
+
+
+
+
+https://shopee.tw/APPLE-iPhone-12-128G-%E7%8F%BE%E8%B2%A8-%E6%94%AF%E6%8F%B45G%E4%B8%8A%E7%B6%B2-A14-CPU-%E9%80%81%E4%BF%9D%E8%AD%B7%E8%B2%BC%E5%85%8C%E6%8F%9B%E5%88%B8-%E7%A5%9E%E8%85%A6%E7%94%9F%E6%B4%BB-i.54598032.4157084237
+
+https://shopee.tw/cart/?itemIds=4157084237&shopId=54598032
+https://shopee.tw/cart/?itemIds=4157084237&shopId=54598032
+https://shopee.tw/cart/?itemIds=4157084237&shopId=54598032
